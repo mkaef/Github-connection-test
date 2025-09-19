fb38bdd6f1785e910a8d4616cd3df28cad29b8b2
mkaef <eric_franck2@yahoo.fr>
2025-09-18 19:38:37 -0700

    This my first change

--- GitTest.java
+++ GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
-        String test = "This a github connection Test";
+        String test = "This a github connection Test, updated with vscode";
 
         System.out.println(test);
     }
